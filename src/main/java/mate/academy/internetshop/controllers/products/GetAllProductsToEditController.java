72c36db4bd9a7eb96f5ac66aeebc02cfe74149e9
NastiaFasova <fasovaaa@fmail.com>
2020-04-30 11:56:16 +0300

    Renamed the controller

--- src/main/java/mate/academy/internetshop/controllers/products/GetAllProductsToEditController.java
+++ src/main/java/mate/academy/internetshop/controllers/products/GetAllProductsToEditController.java
@@ -10,7 +10,7 @@ import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.model.Product;
 import mate.academy.internetshop.service.ProductService;
 
-public class GetAllProductsController extends HttpServlet {
+public class GetAllProductsToEditController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private ProductService productService
             = (ProductService) INJECTOR.getInstance(ProductService.class);
